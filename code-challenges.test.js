d7b13518805a3ea6d52d73ef3ee0c5a4e9c5f78e
Romoramel <romoramel@hotmail.com>
2023-05-31 08:33:05 -0700

    complete

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -14,6 +14,22 @@
 
 // a) Create a test with expect statements using the variables provided.
 
+describe("codedString", () => {
+    it("takes in a string and returns a string with a coded message", () => {
+      expect(codedString(secretCodeWord1)).toEqual("L4ck4d41s1c4l")
+      expect(codedString(secretCodeWord3)).toEqual("G0bbl3dyg00k")
+      expect(codedString(secretCodeWord3)).toEqual("3cc3ntr1c")
+
+      const secretCodeWord1 = "Lackadaisical"
+      // Expected output: "L4ck4d41s1c4l"
+      const secretCodeWord2 = "Gobbledygook"
+      // Expected output: "G0bbl3dyg00k"
+      const secretCodeWord3 = "Eccentric"
+      // Expected output: "3cc3ntr1c"
+
+    })
+  })
+
 const secretCodeWord1 = "Lackadaisical"
 // Expected output: "L4ck4d41s1c4l"
 const secretCodeWord2 = "Gobbledygook"
@@ -23,30 +39,86 @@ const secretCodeWord3 = "Eccentric"
 
 // b) Create the function that makes the test pass.
 
+const codedString = (str) => {
+    let code = {"a":4, "e":3, "i":1, "o":0}
+    return str.split("").map((value) =>{
+        return(object.keys(code).includes(value.toLowercase())) ? code[value.toLowercase()] : value
+    }).join("")
+}
+
+// pseudo code: 
+// input: function that takes in a string
+// output/return: a string with a coded message. The coded message converts "a" to 4, "e" to 3, "i" to 1, and "o" to 0.
+// I created a function that took in a string and returned an array of strings with the split "" and used the .map to iterate through every element in the array. Then i ask for a return and used the .keys method which returns an array of the objects strings. i also used the .includes() method that looks for one string that could be in another string returning the true or false as it should. And the .toLowerCase converts a string to lower case letters
+
+
+
 // --------------------2) Create a function that takes in an array of words and a single letter and returns an array of all the words containing that particular letter.
 
 // a) Create a test with expects statement using the variable provided.
 
-const fruitArray = ["Mango", "Cherry", "Apricot", "Blueberry", "Peach", "Kiwi"]
+describe("letter", () => {
+    it("takes in an array of words and a single letter and returns an array of all the words containing that particular letter", () => {
+      expect(letter(filterletterA)).toEqual(["Mango", "Apricot", "Peach"])
+      expect(letter(filterletterB)).toEqual(["Cherry", "Blueberry", "Peach"])
 
-const filterLetterA = "a"
-// Expected output: ["Mango", "Apricot", "Peach"]
-const filterLetterE = "e"
-// Expected output: ["Cherry", "Blueberry", "Peach"]
+      const filterLetterA = "a"
+      // Expected output: ["Mango", "Apricot", "Peach"]
+      const filterLetterE = "e"
+      // Expected output: ["Cherry", "Blueberry", "Peach"]
+      const fruitArray = ["Mango", "Cherry", "Apricot", "Blueberry", "Peach", "Kiwi"]
+    })
+  })
+
+// pseudo code: 
+// input: array of words and a single letter
+// output/return: array of all the words containing that particular letter.
+// The letter function takes an array of words and a single letter. It returns a new array that only includes the words from the original array that have the specified letter. The function compares the lowercase versions of the words and the letter to make the comparison case-insensitive. The array now has the words that have the specified letter.
 
 // b) Create the function that makes the test pass.
+
+function letter(words, letter) {
+    return words.filter(word => word.toLowerCase().includes(letter.toLowerCase()));
+  }
 
 // --------------------3) Create a function that takes in an array of 5 numbers and determines whether or not the array is a "full house". A full house is exactly one pair and one three of a kind.
 
 // a) Create a test with expect statements using the variable provided.
 
-const hand1 = [5, 5, 5, 3, 3]
-// Expected output: true
-const hand2 = [5, 5, 3, 3, 4]
-// Expected output: false
-const hand3 = [5, 5, 5, 5, 4]
-// Expected output: false
-const hand4 = [7, 2, 7, 2, 7]
-// Expected output: true
+describe("cards", () => {
+    it("takes in an array of 5 numbers and determines whether or not the array is a full house", () => {
+      expect(cards(hand1)).toEqual([true])
+      expect(cards(hand2)).toEqual([false])
+      expect(cards(hand3)).toEqual([false])
+      expect(cards(hand4)).toEqual([true])
+
+
+    })
+  })
+
+  const hand1 = [5, 5, 5, 3, 3]
+        // Expected output: true
+        const hand2 = [5, 5, 3, 3, 4]
+        // Expected output: false
+        const hand3 = [5, 5, 5, 5, 4]
+        // Expected output: false
+        const hand4 = [7, 2, 7, 2, 7]
+        // Expecetd output: true
+
+// pseudo code: 
+// input: a function that takes in an array of 5 numbers
+// output/return: and array determining whether or not the array is a "full house"
+//   The cards function checks if a hand of cards represents a full house or not. It does this by counting the occurrences of each card in the hand and then checking if there are two cards with the same count (2 of a kind) and three cards with the same count (3 of a kind). If conditions are met, it returns true; if not then it returns false.
 
 // b) Create the function that makes the test pass.
+
+function cards(hand) {
+  const counts = {};
+  for (let i = 0; i < hand.length; i++) {
+    const card = hand[i];
+    counts[card] = counts[card] ? counts[card] + 1 : 1;
+  }
+  const cardCounts = Object.values(counts);
+  return cardCounts.includes(2) && cardCounts.includes(3);
+}
+
--- ruby-challenges.rb
+++ ruby-challenges.rb
@@ -10,6 +10,21 @@ filter_letter_o = 'o'
 filter_letter_t = 't'
 # Expected output: ['tea', 'water', 'soda water']
 
+def letter(words, letter)
+    filtered_words = []
+    words.each do |word|
+      if word.include?(letter)
+        filtered_words << word
+      end
+    end
+    filtered_words
+  end
+
+# pseudo code
+# input: a method that takes in an array of words and a single letter 
+# output/return: array of all the words containing that particular letter
+# The function takes in a list of words and a single letter. It starts with an empty list called filteredWords to store the words that have the specified letter. It goes through each word in the list of words. It checks each word if the word contains the letter. If the letter is in the word then it adds the word to the filteredWords list. Then the function returns the filtered words list that has the words that have the specified letter.
+
 
 # -------------------2) Create a method that takes in a hash and returns one array with all the hash values at their own index and in alphabetical order. No nested arrays. Use the test variable provided.
 # HINT: Google 'ruby get rid of nested arrays'
@@ -17,6 +32,16 @@ filter_letter_t = 't'
 us_states = { northwest: ['Washington', 'Oregon', 'Idaho'], southwest: ['California', 'Arizona', 'Nevada'], notheast: ['Maine', 'New Hampshire', 'Rhode Island'] }
 # Expected output: ['Arizona', 'California', 'Idaho', 'Maine', 'Nevada', 'New Hampshire', 'Oregon', 'Rhode Island', 'Washington'] 
 
+def get_sorted_values(hash)
+    values = hash.values.flatten
+    sorted_values = values.sort
+    return sorted_values
+  end
+
+# pseudo code
+# input: a method that takes in a hash
+# output/return: one array with all the hash values at their own index and in alphabetical order. No nested arrays
+# the function collects all the values from the hash and puts them in a list. The list is sorted in alphabetical order from A to Z for the result
 
 # --------------------3a) Create a class called Bike that is initialized with a model, wheels, and current_speed. The default number of wheels is 2. The current_speed should start at 0. Create a bike_info method that returns a sentence with all the data from the bike object.
 
